b1b842078e809bb6d451879487ffc3c8671ec5d6
fernando <fernando@debian>
2017-05-31 18:36:50 -0300

    resolve menu conflito

--- src/pkgAtendimentoRodrigoFernandoGoncalves/Atendimento.java
+++ src/pkgAtendimentoRodrigoFernandoGoncalves/Atendimento.java
@@ -28,11 +28,19 @@ public class Atendimento {
 	}
 
 	public static String menu() {
-		return "MENU DE OPÇÕES\n" + "1 - Recepcionar Cliente\n" + "2 - Consultar clientes a serem atendidos\n"
-				+ "3 - Atender Clientes\n" + "4 - Liberar todos os clientes "
-				+ "5 - Verificar quantidade de clientes a atender\n" + "6 - Localizar cliente por número\n"
-				+ "7 - Localizar cliente por nome\n" + "8 - Emitir relatório de clientes\n9 - Ver Relatórios de clientes\n" + "10 - Filtrar Clientes por valor\n"
-				+ "11 - Ver endereços de hash\n" + "12 - Sobre\n " + "13 - Sair";
+		return "MENU DE OPÇÕES\n" + "1 - Recepcionar Cliente\n" 
+				+ "2 - Consultar clientes a serem atendidos\n"
+				+ "3 - Atender Clientes\n" 
+				+ "4 - Liberar todos os clientes\n"
+				+ "5 - Verificar quantidade de clientes a atender\n" 
+				+ "6 - Localizar cliente por número\n"
+				+ "7 - Localizar cliente por nome\n" 
+				+ "8 - Emitir relatório de clientes\n"
+				+ "9 - Ver Relatórios de clientes\n" 
+				+ "10 - Filtrar Clientes por valor\n"
+				+ "11 - Ver endereços de hash\n" 
+				+ "12 - Sobre\n " 
+				+ "13 - Sair";
 	}
 
 	// metodo abaixo verifica se a lista contem numero do cartão cadastrado
